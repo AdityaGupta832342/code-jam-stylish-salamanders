c3c0c0354a00f49ff0a2345a4f61b2021685a6ea
Vedant Shenoy <vedant27shenoy@gmail.com>
2021-07-14 22:12:38 +0530

    Adds basic click/keypress funcitionality
    Left-click: draw point
    Double-click: draw marker (rectangle)
    Right-click: draw lines
    q: exit
    c: clear
    pyautogui hack to zoom out

--- black_board.py
+++ black_board.py
@@ -1,22 +1,42 @@
-from time import sleep
-
 from asciimatics.screen import ManagedScreen
+# import pyautogui
 
 
 def demo() -> None:
-    """Wrapper function for creating a screen as black board."""
-    a = None
+    """Wrapper function for a blackboard"""
+    stack = []
     with ManagedScreen() as screen:
-        while a is None:
+        while True:
             screen.wait_for_input(30)
             a = screen.get_event()
-            if a.buttons in (1, 4):
-                screen.print_at("*", a.x, a.y)
-                screen.refresh()
-                a = None
+
+            if hasattr(a, 'key_code'):
+                if a.key_code == 99:  # c
+                    screen.clear()
+                    stack.clear()
+                elif a.key_code == 113:  # q
+                    break
+            elif hasattr(a, 'buttons'):
+                if a.buttons == 2:  # right_click
+                    stack.append([a.x, a.y])
+                    if len(stack) >= 2:
+                        screen.move(*stack[-2])
+                        screen.draw(*stack[-1], char='.', thin=True)
+                elif a.buttons == 0:  # scroll/triple_left/double_right
+                    stack.clear()
+                elif a.buttons == 4:  # double_left
+                    for i in range(-1, 2):
+                        for j in range(-1, 2):
+                            screen.print_at('*', a.x + i, a.y + j)
+                else:  # left_click
+                    screen.print_at('*', a.x, a.y)
             else:
-                a = "yo"
-    sleep(3)
+                screen.print_at("Didn't detect key press or button")
+            screen.refresh()
 
 
+# Hack for small fontsize on Ubuntu
+# for i in range(7):
+#     pyautogui.hotkey('ctrl', '-')
 demo()
+# pyautogui.hotkey('ctrl', '0')
